9c2243bd6155777fca8d0ad29defb2ea7ad023ee
SergioRt1 <sergio200035@gmail.com>
2019-05-15 00:05:22 -0500

    Import and config google maps API services, add deep linking

--- app/src/main/java/com/ieti/easywheels/ui/MapsActivity.java
+++ app/src/main/java/com/ieti/easywheels/ui/MapsActivity.java
@@ -1,8 +1,15 @@
 package com.ieti.easywheels.ui;
 
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.View;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -10,22 +17,69 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.maps.DirectionsApiRequest;
+import com.google.maps.GeoApiContext;
+import com.google.maps.errors.ApiException;
+import com.google.maps.model.DirectionsResult;
+import com.google.maps.model.TravelMode;
 import com.ieti.easywheels.R;
+
+import java.io.IOException;
+import java.util.Date;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
 
+
+    // Keys for storing activity state.
+    private static final String KEY_CAMERA_POSITION = "camera_position";
+    private static final String KEY_LOCATION = "location";
+
+    // Map vars
+    private final LatLng mDefaultLocation = new LatLng(-33.8523341, 151.2106085);
+    private static final int DEFAULT_ZOOM = 15;
+
+    //Google API
+    private GeoApiContext mGeoApiContext;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Retrieve location and camera position from saved instance state.
+        if (savedInstanceState != null) {
+//            mLastKnownLocation = savedInstanceState.getParcelable(KEY_LOCATION);
+//            mCameraPosition = savedInstanceState.getParcelable(KEY_CAMERA_POSITION);
+        }
+
         setContentView(R.layout.activity_maps);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+
+        initGoogleMap();
+
     }
 
+    private void initGoogleMap(){
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        if(mGeoApiContext == null){
+            String apiKey = getString(R.string.google_maps_key);
+            mGeoApiContext = new GeoApiContext.Builder().apiKey(apiKey).build();
+        }
+    }
+
+    /**
+     * Saves the state of the map when the activity is paused.
+     */
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        if (mMap != null) {
+            outState.putParcelable(KEY_CAMERA_POSITION, mMap.getCameraPosition());
+//            outState.putParcelable(KEY_LOCATION, mLastKnownLocation);
+            super.onSaveInstanceState(outState);
+        }
+    }
 
     /**
      * Manipulates the map once available.
@@ -44,5 +98,25 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+    public void calculateRoute(TravelMode travelMode, LatLng origin, LatLng destination){
+        DirectionsApiRequest directionsApiRequest = new DirectionsApiRequest(mGeoApiContext);
+        try {
+            DirectionsResult result = directionsApiRequest.origin(
+                    new com.google.maps.model.LatLng(origin.latitude,origin.longitude)
+            ).destination(
+                    new com.google.maps.model.LatLng(destination.latitude,destination.longitude)
+            ).mode(travelMode)
+//            .departureTime()
+                    .await();
+        } catch (ApiException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
---
+++ app/src/main/java/com/ieti/easywheels/util/Constants.java
@@ -0,0 +1,7 @@
+package com.ieti.easywheels.util;
+
+public class Constants {
+
+    public static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 9003;
+    public static final int PERMISSIONS_REQUEST_ENABLE_GPS = 9002;
+}
