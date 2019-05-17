4870b0b47a4c0f12487fdf7dc3e3aacd9f029b98
David <david.rodriguez-a@mail.escuelaing.edu.co>
2019-05-16 22:37:13 -0500

    Informacion on trip cards on trips view

--- app/src/main/java/com/ieti/easywheels/util/AdapterUtils.java
+++ app/src/main/java/com/ieti/easywheels/util/AdapterUtils.java
@@ -5,6 +5,10 @@ import android.location.Location;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.firebase.firestore.GeoPoint;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class AdapterUtils {
     public static LatLng convertLocationToLatLng(Location location) {
         return new LatLng(location.getLatitude(), location.getLongitude());
@@ -24,6 +28,25 @@ public class AdapterUtils {
 
     public static LatLng convertGeoPointToLatLng(GeoPoint latLng) {
         return new LatLng(latLng.getLatitude(), latLng.getLongitude());
+    }
+
+    public static List<GeoPoint> convertHasMapToGeopoint(List<HashMap<String,Double>> list){
+        List<GeoPoint> geoPoints = new ArrayList<>();
+        for(HashMap<String,Double> hashMap:list){
+            geoPoints.add(new GeoPoint(hashMap.get("lat"),hashMap.get("lng")));
+        }
+        return geoPoints;
+    }
+
+    public static List<HashMap<String,Double>> convertGeoPointsIntoHashMap(List<GeoPoint> geoPoints){
+        List<HashMap<String,Double>> list = new ArrayList<>();
+        for(GeoPoint g:geoPoints){
+            HashMap<String,Double> hashMap = new HashMap<>();
+            hashMap.put("lat",g.getLatitude());
+            hashMap.put("lng",g.getLongitude());
+            list.add(hashMap);
+        }
+        return list;
     }
 
 
